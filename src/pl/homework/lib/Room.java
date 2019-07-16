ecc12aa3976d231ded8460a4d58788fc58aca25e
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-16 20:56:02 +0200

    I created method temperatureReduction() in the Room class. This methos was tested for room , room2 and room3 objects (RoomTests class)

--- src/pl/homework/lib/Room.java
+++ src/pl/homework/lib/Room.java
@@ -1,5 +1,7 @@
 package pl.homework.lib;
 
+import static pl.homework.app.RoomTest.MIN_TEMPERATURE;
+
 public class Room {
     private double surfaceSize;
     private double temperature;
@@ -19,26 +21,42 @@ public class Room {
     }
 
     public double getSurfaceSize() {
+
         return surfaceSize;
     }
 
     public void setSurfaceSize(double surfaceSize) {
+
         this.surfaceSize = surfaceSize;
     }
 
     public double getTemperature() {
+
         return temperature;
     }
 
     public void setTemperature(double temperature) {
+
         this.temperature = temperature;
     }
 
     public boolean isAirConditioner() {
+
         return airConditioner;
     }
 
     public void setAirConditioner(boolean airConditioner) {
+
         this.airConditioner = airConditioner;
+    }
+
+    public boolean temperatureReduction() {
+        if (getTemperature() > MIN_TEMPERATURE && isAirConditioner()) {
+            setTemperature(getTemperature()-1);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
